1bb914ae7224cd90ea95fcfd95be09d660629d91
Roy Keyes <keyes@ufl.edu>
2017-02-10 15:30:32 -0500

    Fix the filter_raw scripts to better handle copy and backup of the data they are working with

--- filter_raw.sh
+++ filter_raw.sh
@@ -14,13 +14,36 @@ else
         exit
 fi
 
-if [ ! -z ${PROJECT_ROOT} ] && [ ! -z ${RAW_FILE} ];
-then
-    echo "Pre-filtering raw data after retrieval from the SFTP server"
-    python ${MY_DIR}/filter_raw.py ${RAW_FILE} ${FILTERED_FILE} ${MY_DIR}/ ${RAW_FILTER_PATH}
+function listFiles() {
+    echo "Showing all generated and parsed files in their current state"
+    ls_flags=al
+    ls -${ls_flags} ${RAW_FILE}
+    md5 ${RAW_FILE}
+    ls -${ls_flags} ${FILTERED_FILE}
+    md5 ${FILTERED_FILE}
+    ls -${ls_flags} $OLD_RAW_FILE
+    md5 ${OLD_RAW_FILE}
+}
 
-    echo "Moving filtered file to rename as original raw file name"
-    mv ${FILTERED_FILE} ${RAW_FILE}
+if [ ! -z ${PROJECT_ROOT} ] && [ ! -z ${RAW_FILTER_PATH} ] && [ ! -z ${RAW_FILE} ] && [ ! -z ${FILTERED_FILE} ] && [ ! -z ${OLD_RAW_FILE} ];
+then
+    if [ -f ${RAW_FILE} ];
+    then
+        listFiles
+
+        echo "Pre-filtering raw data [${RAW_FILE}], outputting to [${FILTERED_FILE}]"
+        python ${MY_DIR}/filter_raw.py ${RAW_FILE} ${FILTERED_FILE} ${MY_DIR}/ ${RAW_FILTER_PATH}
+
+        echo "Copying old raw file [${RAW_FILE}] to backup copy [${OLD_RAW_FILE}]"
+        cp ${RAW_FILE} ${OLD_RAW_FILE}
+
+        echo "Moving filtered file [${FILTERED_FILE}] to rename as original raw file name [${RAW_FILE}]"
+        cp ${FILTERED_FILE} ${RAW_FILE}
+
+        listFiles
+    else
+        echo "You are missing your raw_file, specified in config as [${RAW_FILE}]"
+    fi
 else
-    echo "Missing PROJECT_ROOT and/or RAW_FILE in your config"
+    echo "Missing PROJECT_ROOT, RAW_FILTER_PATH, RAW_FILE, FILTERED_FILE, or OLD_RAW_FILE in your config"
 fi
